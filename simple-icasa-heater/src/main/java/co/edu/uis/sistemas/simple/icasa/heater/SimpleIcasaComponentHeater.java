6d32a5bbae670d3a76fb4c6eaa7d66a93ad1baea
jams2411 <jams_2411@hotmail.com>
2013-04-26 12:05:42 -0500

    Código comentado y arreglado

--- simple-icasa-heater/src/main/java/co/edu/uis/sistemas/simple/icasa/heater/SimpleIcasaComponentHeater.java
+++ simple-icasa-heater/src/main/java/co/edu/uis/sistemas/simple/icasa/heater/SimpleIcasaComponentHeater.java
@@ -23,77 +23,56 @@ import fr.liglab.adele.icasa.device.temperature.Thermometer;
 @Instantiate
 public class SimpleIcasaComponentHeater implements DeviceListener{
 	
+	//Para listar los calentadores de la casa
 	@Requires(id="heaters")
 	private Heater[] heaters;
 	
+	//Para escuchar los eventos de cambio de propiedad de los termometros
 	@Requires(id="thermometers")
 	private Thermometer[] thermometers;
 	
+	//Para listar los enfriadores de la casa
 	@Requires(id="coolers")
 	private Cooler[] coolers;
 	
 	
+	//Cuando se agrega un termometro se pone a escuchar el cambio de sus propiedades!
 	@Bind(id="thermometers")
 	protected void bindThermometer(Thermometer thermometer) {
 		thermometer.addListener(this);
-		System.out.println("Escucha el evento");
 	}
 	
+	//Cuando se elimina un termometro se deja a escuchar el cambio de sus propiedades!
 	@Unbind(id="thermometers")
 	protected void unbindThermometers(Thermometer thermometer)
 	{
+		//Se apagan los calentadores y enfriadores del cuarto donde está el termometro que se quito
+		for (Heater heater : heaters)
+		{
+			if(heater.getPropertyValue(GenericDevice.LOCATION_PROPERTY_NAME) == thermometer.getPropertyValue(GenericDevice.LOCATION_PROPERTY_NAME))
+				heater.setPowerLevel(0.0);
+		}
+		
+		
+		for (Cooler cooler : coolers)
+		{
+			//El enfriador de la habitación donde está el termometro debe ser apagado
+			if(cooler.getPropertyValue(GenericDevice.LOCATION_PROPERTY_NAME) == thermometer.getPropertyValue(GenericDevice.LOCATION_PROPERTY_NAME))
+				cooler.setPowerLevel(0.0);
+		}
+		
 		thermometer.removeListener(this);
-		System.out.println("Deja de escuchar el evento");
 	}
 
+	//Lista los calentadores de la casa
 	protected List<Heater> getHeaters() {
 		return Collections.unmodifiableList(Arrays.asList(heaters));
 	}
 	
-	protected List<Thermometer> getThermometers() {
-		return Collections.unmodifiableList(Arrays.asList(thermometers));
-	}
-	
+	//Lista los enfriadores de la casa
 	protected List<Cooler> getCoolers() {
 		return Collections.unmodifiableList(Arrays.asList(coolers));
 	}
-
-	
-	@Validate
-	public void start() {
-		/*modifyHeatersThread = new Thread(new ModifyHeatersRunnable());
-		modifyHeatersThread.start();*/
-	}
-	
-	@Invalidate
-	public void stop() throws InterruptedException {
-		/*modifyHeatersThread.interrupt();
-		modifyHeatersThread.join();*/
-	}
-
-	
-	/*class ModifyHeatersRunnable implements Runnable {
-
-		public void run() {
-						
-			boolean running = true;
-			
-			while (running) {
-				//try {
-					List<Heater> heaters = getHeaters();
-					for (Heater heater : heaters) {
-						heater.setPowerLevel(0.2);
-					}
-					//Thread.sleep(1000);					
-				/*} catch (InterruptedException e) {
-					running = false;
-				}
-			}
-			
-		}
-		
-	}*/
-
 
 	public void deviceAdded(GenericDevice arg0) {
 		// TODO Auto-generated method stub
@@ -105,41 +84,51 @@ public class SimpleIcasaComponentHeater implements DeviceListener{
 		
 	}
 
+	//Evento que se ejecuta cuando a los termometros se les afecta alguna propiedad
 	public void devicePropertyModified(GenericDevice device, String property, Object value) {
-		// TODO Auto-generated method stub
+		
+		//Se asegura que la pripiedad que se modificó sea la temperatura
 		if(property == Thermometer.THERMOMETER_CURRENT_TEMPERATURE)
 		{
+			//Obtiene La lista de calentadores y enfriadores
 			List<Heater> heaters = getHeaters();
 			List<Cooler> coolers = getCoolers();
+			//La temperatura es muy baja?
 			if(Double.parseDouble(value.toString()) <= 290)
 			{
+				//Recorre todos los heaters
 				for (Heater heater : heaters) {
+					//La localización del calentador es la misma que el termometro que bajo de temperatura?
 					if(heater.getPropertyValue(GenericDevice.LOCATION_PROPERTY_NAME) == device.getPropertyValue(GenericDevice.LOCATION_PROPERTY_NAME))
 					{
+						//Empieza a calentar
 						heater.setPowerLevel(1.0);
+						//Recorre todos los enfriadores
 						for (Cooler cooler : coolers)
 						{
+							//El enfriador de la habitación donde está el termometro debe ser apagado
 							if(cooler.getPropertyValue(GenericDevice.LOCATION_PROPERTY_NAME) == device.getPropertyValue(GenericDevice.LOCATION_PROPERTY_NAME))
-							{
 								cooler.setPowerLevel(0.0);
-							}
 						}
 					}
 				}
 			}
+			//Se elevó la temperatura?
 			else if(Double.parseDouble(value.toString()) >= 300)
 			{
+				//Recorro los enfriadores
 				for (Cooler cooler : coolers) 
 				{
+					//El enfriador está en la misma localización del termometro?
 					if(cooler.getPropertyValue(GenericDevice.LOCATION_PROPERTY_NAME) == device.getPropertyValue(GenericDevice.LOCATION_PROPERTY_NAME))
 					{
+						//Empieza a enfriar
 						cooler.setPowerLevel(1.0);
+						//Recorre los calentadores para apagr el que esté en la misma habitación del termometro
 						for (Heater heater : heaters)
 						{
 							if(heater.getPropertyValue(GenericDevice.LOCATION_PROPERTY_NAME) == device.getPropertyValue(GenericDevice.LOCATION_PROPERTY_NAME))
-							{
 								heater.setPowerLevel(0.0);
-							}
 						}
 					}
 				}
