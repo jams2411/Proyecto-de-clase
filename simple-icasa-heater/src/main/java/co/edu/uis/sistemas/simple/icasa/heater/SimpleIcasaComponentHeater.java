b6c82a90ad966c001289e2eb623d33943f647715
jams2411 <jams_2411@hotmail.com>
2013-04-26 09:21:35 -0500

    Clase final

--- simple-icasa-heater/src/main/java/co/edu/uis/sistemas/simple/icasa/heater/SimpleIcasaComponentHeater.java
+++ simple-icasa-heater/src/main/java/co/edu/uis/sistemas/simple/icasa/heater/SimpleIcasaComponentHeater.java
@@ -9,35 +9,59 @@ import org.apache.felix.ipojo.annotations.Component;
 import org.apache.felix.ipojo.annotations.Instantiate;
 import org.apache.felix.ipojo.annotations.Invalidate;
 import org.apache.felix.ipojo.annotations.Requires;
+import org.apache.felix.ipojo.annotations.Unbind;
 import org.apache.felix.ipojo.annotations.Validate;
 
-import fr.liglab.adele.icasa.device.light.BinaryLight;
+import fr.liglab.adele.icasa.device.DeviceListener;
+import fr.liglab.adele.icasa.device.GenericDevice;
+import fr.liglab.adele.icasa.device.temperature.Cooler;
 import fr.liglab.adele.icasa.device.temperature.Heater;
+import fr.liglab.adele.icasa.device.temperature.Thermometer;
 
 
 @Component(name="SimpleIcasaComponentHeater")
 @Instantiate
-public class SimpleIcasaComponentHeater {
+public class SimpleIcasaComponentHeater implements DeviceListener{
 	
 	@Requires(id="heaters")
 	private Heater[] heaters;
 	
+	@Requires(id="thermometers")
+	private Thermometer[] thermometers;
+	
+	@Requires(id="coolers")
+	private Cooler[] coolers;
+	
 	private Thread modifyHeatersThread;
 	
-	@Bind(id="heaters")
-	protected void bindLight(Heater Heater) {
-		System.out.println("A new heater has been added to the platform " + Heater.getSerialNumber());
+	@Bind(id="thermometers")
+	protected void bindThermometer(Thermometer thermometer) {
+		thermometer.addListener(this);
+	}
+	
+	@Unbind(id="thermometers")
+	protected void unbindThermometers(Thermometer thermometer)
+	{
+		thermometer.removeListener(this);
 	}
 
 	protected List<Heater> getHeaters() {
 		return Collections.unmodifiableList(Arrays.asList(heaters));
 	}
+	
+	protected List<Thermometer> getThermometers() {
+		return Collections.unmodifiableList(Arrays.asList(thermometers));
+	}
+	
+	protected List<Cooler> getCoolers() {
+		return Collections.unmodifiableList(Arrays.asList(coolers));
+	}
 
 	
 	@Validate
 	public void start() {
-		modifyHeatersThread = new Thread(new ModifyHeatersRunnable());
-		modifyHeatersThread.start();
+		/*modifyHeatersThread = new Thread(new ModifyHeatersRunnable());
+		modifyHeatersThread.start();*/
 	}
 	
 	@Invalidate
@@ -47,15 +71,13 @@ public class SimpleIcasaComponentHeater {
 	}
 
 	
-	class ModifyHeatersRunnable implements Runnable {
+	/*class ModifyHeatersRunnable implements Runnable {
 
 		public void run() {
 						
 			boolean running = true;
 			
-			boolean onOff = false;
 			while (running) {
-				onOff = !onOff;
 				//try {
 					List<Heater> heaters = getHeaters();
 					for (Heater heater : heaters) {
@@ -64,10 +86,73 @@ public class SimpleIcasaComponentHeater {
 					//Thread.sleep(1000);					
 				/*} catch (InterruptedException e) {
 					running = false;
-				}*/
+				}
 			}
 			
 		}
+		
+	}*/
+
+
+	public void deviceAdded(GenericDevice arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void devicePropertyAdded(GenericDevice arg0, String arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void devicePropertyModified(GenericDevice device, String property, Object value) {
+		// TODO Auto-generated method stub
+		if(property == Thermometer.THERMOMETER_CURRENT_TEMPERATURE)
+		{
+			List<Heater> heaters = getHeaters();
+			List<Cooler> coolers = getCoolers();
+			if(Double.parseDouble(value.toString()) <= 290)
+			{
+				for (Heater heater : heaters) {
+					if(heater.getPropertyValue(GenericDevice.LOCATION_PROPERTY_NAME) == device.getPropertyValue(GenericDevice.LOCATION_PROPERTY_NAME))
+					{
+						heater.setPowerLevel(0.2);
+						for (Cooler cooler : coolers)
+						{
+							if(cooler.getPropertyValue(GenericDevice.LOCATION_PROPERTY_NAME) == device.getPropertyValue(GenericDevice.LOCATION_PROPERTY_NAME))
+							{
+								cooler.setPowerLevel(0.0);
+							}
+						}
+					}
+				}
+			}
+			else if(Double.parseDouble(value.toString()) >= 300)
+			{
+				for (Cooler cooler : coolers) 
+				{
+					if(cooler.getPropertyValue(GenericDevice.LOCATION_PROPERTY_NAME) == device.getPropertyValue(GenericDevice.LOCATION_PROPERTY_NAME))
+					{
+						cooler.setPowerLevel(0.2);
+						for (Heater heater : heaters)
+						{
+							if(heater.getPropertyValue(GenericDevice.LOCATION_PROPERTY_NAME) == device.getPropertyValue(GenericDevice.LOCATION_PROPERTY_NAME))
+							{
+								heater.setPowerLevel(0.0);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	public void devicePropertyRemoved(GenericDevice arg0, String arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deviceRemoved(GenericDevice arg0) {
+		// TODO Auto-generated method stub
 		
 	}
 	
